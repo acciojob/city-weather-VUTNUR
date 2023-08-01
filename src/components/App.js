735b0a6b6f6cd56852cd870e53b8717fec6c12d9
VUTNUR <vutnurnethajigoud@gmail.com>
2023-08-02 00:30:56 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
+
 import React from "react";
 import './../styles/App.css';
+import Fetch from "./Fetch";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Fetch />
     </div>
   )
 }
